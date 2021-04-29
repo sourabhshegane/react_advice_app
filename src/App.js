d3dc0edcc44cb6ede094089d2d73856457bedd06
sourabhshegane <sgsourabh30@gmail.com>
2021-04-29 16:09:50 +0530

    Intialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, {useState} from 'react'
 
 function App() {
+
+  const [advice, setAdvice] = useState('')
+  
+  const getAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice')
+    .then(res => {
+      setAdvice(res.data.slip.advice)
+      console.log(res.data.slip.advice)
+    }).catch(error => console.log(error))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="main_container">
+        <div className="sub_main_container">
+          <div className="adv_container">
+              <h1 className="adv">{advice}</h1>
+          </div>
+          <button className="adv_btn" onClick={getAdvice}>
+            Get advice
+          </button> 
+          </div>
+          <h2>Made with ❤️ by Sourabh Shegane</h2>
+        </div>
     </div>
   );
 }
